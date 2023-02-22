5224ae49053eb66ec83448ee86038cd65c3e1242
MiyuYokoyama0109 <miyu.y@latona.io>
2023-02-22 13:12:50 +0900

    release

--- data-platform-partner-function-sql-partner-function-data-setup.sql
+++ data-platform-partner-function-sql-partner-function-data-setup.sql
@@ -2,17 +2,17 @@ LOCK TABLES `data_platform_partner_function_partner_function_data` WRITE;
 
 INSERT INTO `data_platform_partner_function_partner_function_data` (`PartnerFunction`)
 VALUES
-    ('BUYER');
-    ('SELLER');
-    ('CUSTOMER');
-    ('SUPPLIER');
-    ('MANUFACTURER');
-    ('DELIVERFROM');
-    ('DELIVERTO');
-    ('LOGI');
-    ('BILLTO');
-    ('BILLFROM');
-    ('PAYER');
-    ('PAYEE');
+    ('BUYER'),
+    ('SELLER'),
+    ('CUSTOMER'),
+    ('SUPPLIER'),
+    ('MANUFACTURER'),
+    ('DELIVERFROM'),
+    ('DELIVERTO'),
+    ('LOGI'),
+    ('BILLTO'),
+    ('BILLFROM'),
+    ('PAYER'),
+    ('PAYEE'),
     ('PSPROVIDER');
 UNLOCK TABLES;
